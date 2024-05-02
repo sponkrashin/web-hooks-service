52efbc244be9b44ad2a8abbe40635f89b3ec3f16
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-05-03 01:55:12 +0300

    Add simple web hooks middleware

--- WebHooksService/WebHooksService/Program.cs
+++ WebHooksService/WebHooksService/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using WebHooksService;
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddYamlFile("web-hooks.yaml", true);
+builder.Services.AddScoped<WebHooksMiddleware>();
 
 var app = builder.Build();
+app.UseMiddleware<WebHooksMiddleware>();
 app.Run();
---
+++ WebHooksService/WebHooksService/WebHooksMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace WebHooksService;
+
+public class WebHooksMiddleware : IMiddleware
+{
+    private readonly IConfiguration configuration;
+
+    public WebHooksMiddleware(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var path = context.Request.Path.Value!;
+        if (path.StartsWith('/') && path.Length != 1)
+        {
+            path = path[1..];
+        }
+
+        var webHookConfiguration = this.configuration.GetSection($"web-hooks:{path}");
+        if (!webHookConfiguration.Exists())
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            return Task.CompletedTask;
+        }
+
+        context.Response.StatusCode = StatusCodes.Status202Accepted;
+        return Task.CompletedTask;
+    }
+}
