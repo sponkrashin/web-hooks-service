5948d4b31077e19e07f39c768541437ceb7e2109
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-05-03 03:06:03 +0300

    Implement web hooks middleware

--- WebHooksService/WebHooksService/WebHooksMiddleware.cs
+++ WebHooksService/WebHooksService/WebHooksMiddleware.cs
@@ -1,5 +1,7 @@
 ﻿namespace WebHooksService;
 
+using System.Diagnostics;
+
 public class WebHooksMiddleware : IMiddleware
 {
     private readonly IConfiguration configuration;
@@ -9,7 +11,7 @@ public class WebHooksMiddleware : IMiddleware
         this.configuration = configuration;
     }
 
-    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var path = context.Request.Path.Value!;
         if (path.StartsWith('/') && path.Length != 1)
@@ -21,10 +23,23 @@ public class WebHooksMiddleware : IMiddleware
         if (!webHookConfiguration.Exists())
         {
             context.Response.StatusCode = StatusCodes.Status404NotFound;
-            return Task.CompletedTask;
+            return;
         }
 
-        context.Response.StatusCode = StatusCodes.Status202Accepted;
-        return Task.CompletedTask;
+        try
+        {
+            var processArgs = string.Join(" && ", webHookConfiguration.Get<IEnumerable<string>>() ?? Enumerable.Empty<string>());
+            var processStartInfo = new ProcessStartInfo("cmd", $"/C \"{processArgs}\"");
+
+            var process = Process.Start(processStartInfo);
+            await process!.WaitForExitAsync();
+
+            context.Response.StatusCode = StatusCodes.Status202Accepted;
+        }
+        catch (Exception ex)
+        {
+            await context.Response.WriteAsync(ex.ToString());
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
     }
 }
