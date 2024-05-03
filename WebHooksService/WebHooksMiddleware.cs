e9aaf1890345c9dee42938a8ad2bf882c9fdfdc0
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-05-03 23:32:33 +0300

    Add logging

--- WebHooksService/WebHooksMiddleware.cs
+++ WebHooksService/WebHooksMiddleware.cs
@@ -5,10 +5,12 @@ using System.Diagnostics;
 public class WebHooksMiddleware : IMiddleware
 {
     private readonly IConfiguration configuration;
+    private readonly ILogger<WebHooksMiddleware> logger;
 
-    public WebHooksMiddleware(IConfiguration configuration)
+    public WebHooksMiddleware(IConfiguration configuration, ILogger<WebHooksMiddleware> logger)
     {
         this.configuration = configuration;
+        this.logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
@@ -19,37 +21,61 @@ public class WebHooksMiddleware : IMiddleware
             path = path[1..];
         }
 
-        var webHookConfiguration = this.configuration.GetSection($"WebHooks:{path}");
+        this.logger.LogDebug("Path: {Path}", path);
+
+        var webHookConfigurationSectionName = $"WebHooks:{path}";
+
+        this.logger.LogDebug("Web hook configuration section name: {SectionName}", webHookConfigurationSectionName);
+
+        var webHookConfiguration = this.configuration.GetSection(webHookConfigurationSectionName);
         if (!webHookConfiguration.Exists())
         {
+            this.logger.LogInformation("Configuration for web hook with name {WebHookName} does not exist", path);
             context.Response.StatusCode = StatusCodes.Status404NotFound;
             return;
         }
+
+        this.logger.LogInformation("Configuration for web hook with name {WebHookName} exists", path);
 
         try
         {
             var webHookCommands = webHookConfiguration.Get<IEnumerable<string>>() ?? Enumerable.Empty<string>();
 
-            ProcessStartInfo processStartInfo;
+            this.logger.LogDebug("Web hook commands: {@WebHookCommands}", webHookCommands);
+
+            this.logger.LogInformation("Environment: {Environment}", OperatingSystem.IsWindows() ? "Windows" : "Linux");
+
+            string command;
+            string commandArgs;
 
             if (OperatingSystem.IsWindows())
             {
-                var processArgs = string.Join(" && ", webHookCommands);
-                processStartInfo = new ProcessStartInfo("cmd", $"/C \"{processArgs}\"");
+                command = "cmd";
+                commandArgs = $"/C \"{string.Join(" && ", webHookCommands)}\"";
             }
             else
             {
-                var processArgs = string.Join(";", webHookCommands);
-                processStartInfo = new ProcessStartInfo("bash", $"-c \"{processArgs}\"");
+                command = "bash";
+                commandArgs = $"-c \"{string.Join(";", webHookCommands)}\"";
             }
 
+            this.logger.LogDebug("Running command {Command} {@Arguments}", command, commandArgs);
+
+            var processStartInfo = new ProcessStartInfo(command, commandArgs);
+
             var process = Process.Start(processStartInfo);
+
+            this.logger.LogInformation("Process has successfully started");
+
             await process!.WaitForExitAsync();
+
+            this.logger.LogInformation("Process has successfully finished");
 
             context.Response.StatusCode = StatusCodes.Status202Accepted;
         }
         catch (Exception ex)
         {
+            this.logger.LogError(ex, "An error occurred during the request");
             await context.Response.WriteAsync(ex.ToString());
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
         }
