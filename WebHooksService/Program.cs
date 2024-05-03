8902b8faf5d1487816e8b700390930688c4d0a47
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-05-03 23:59:22 +0300

    Add configuration for nginx

--- WebHooksService/Program.cs
+++ WebHooksService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 using WebHooksService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,11 @@ builder.Configuration.AddJsonFile("web-hooks.json", true);
 builder.Services.AddScoped<WebHooksMiddleware>();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.MapShortCircuit(404, "favicon.ico");
 app.UseMiddleware<WebHooksMiddleware>();
