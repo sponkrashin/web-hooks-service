1b91b25e315d9d277288db15de16b7475b530607
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-05-03 23:33:45 +0300

    Add short-circuit for favicon file

--- WebHooksService/Program.cs
+++ WebHooksService/Program.cs
@@ -5,5 +5,8 @@ builder.Configuration.AddJsonFile("web-hooks.json", true);
 builder.Services.AddScoped<WebHooksMiddleware>();
 
 var app = builder.Build();
+
+app.MapShortCircuit(404, "favicon.ico");
 app.UseMiddleware<WebHooksMiddleware>();
+
 app.Run();
